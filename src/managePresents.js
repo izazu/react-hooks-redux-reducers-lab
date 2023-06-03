441b3ec042933b058612807de3dabee6f380a6cc
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-06-03 12:58:40 -0600

    done

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,13 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  if (action.type === "presents/increase") {
+    return {
+      ...state,
+      numberOfPresents: state.numberOfPresents + 1,
+    };
+  }
+
+  // Return the previous state for any other type of action
+  return state;
 }
